e80c708a0b946ed109cecabec75b1bf564d120b5
wolf mr <mozhongwen93@foxmail.com>
2020-04-21 14:47:05 +0800

    2020.4.21 --2

--- back.js
+++ back.js
@@ -18,43 +18,42 @@ app.use(
 app.get("/test", function(req, res) {
   res.header("Access-Control-Allow-Origin", "*"); //允许全网跨域访问
   //这是一个回调函数
-  connectMysql(
-    getinfo(function(result) {
-      res.send(result);
-    })
-  );
+
+  mysqlFuc(function(result) {
+    res.send(result);
+  });
 });
 
 //监听666端口
 var server = app.listen(666, function() {});
 
-//连接数据库（回调写法，保守原则）
-function connectMysql(next) {
-  //连接数据库
-  var connection = mysql.createConnection({
-    host: "118.24.179.175", //数据库域名，localhost表示同域名，也就是同一台服务器
-    user: "root", //数据库用户名
-    password: "123456", //数据库密码
-    port: "3306", //数据库域名监听的端口
-    database: "cloundlisten", //调用的数据库中具体的库
-  });
-  connection.connect(); //链接数据库
-  next();
-}
+//调用数据库的函数
 
-//调用数据库的函数(测试)
-function getinfo(next) {
-  //获取当前库中表的名称
-  var sql = "select * from userInfo;";
-  connection.query(sql, function(err, result) {
-    if (err) {
-      console.log(err);
-      console.log("error!");
-      next("error!");
-    } else {
-      console.log("获取数据库返回数据");
-      console.log(result);
-      next(result);
-    }
-  });
+//mysql封装函数
+function mysqlFuc(next) {
+  getinfo(next);
+  function getinfo(next) {
+    //连接数据库
+    var connection = mysql.createConnection({
+      host: "118.24.179.175", //数据库域名，localhost表示同域名，也就是同一台服务器
+      user: "root", //数据库用户名
+      password: "123456", //数据库密码
+      port: "3306", //数据库域名监听的端口
+      database: "cloundlisten", //调用的数据库中具体的库
+    });
+    connection.connect(); //链接数据库
+    //获取当前库中表的名称
+    var sql = "select * from userInfo;";
+    connection.query(sql, function(err, result) {
+      if (err) {
+        console.log(err);
+        console.log("error!");
+        next("error!");
+      } else {
+        console.log("获取数据库返回数据");
+        console.log(result);
+        next(result);
+      }
+    });
+  }
 }
